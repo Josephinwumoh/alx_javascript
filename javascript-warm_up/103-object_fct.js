ee72df0c9216b38fa62f29bef628c7ef8608edda
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-05 04:32:05 +0100

    Increment Object.

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -9,9 +9,12 @@ console.log(myObject);
 myObject.incr = function () {
     this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
