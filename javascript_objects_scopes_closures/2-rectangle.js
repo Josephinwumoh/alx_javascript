d73295c420523fd8f81551da7cf52642cc4b4ff5
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-06 03:32:38 +0100

    An Empty Object.

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,11 @@
 
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // If w or h is not a positive integer or equal to 0, create an empty object
-        return {};
-      } else {
+      if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       }
     }
-  }
-  
+}
+
 module.exports = Rectangle;  /* Export the Rectangle class*/
