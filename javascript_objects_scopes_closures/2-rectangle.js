fe2120b43a095ba236bd7325229a556df7d9ef5a
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-06 03:03:21 +0100

    Return an Empty Object.

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,9 @@
 
 class Rectangle {
     constructor(w, h) {
+        
         if (w <= 0 || h <= 0) {
-            /* if w or h is equal to 0 or not a positive integer,
-            create an empty object*/
+
             return {};
         }   else {
             this.width = w;
