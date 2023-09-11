d0bc00a320be647afc1fa1dc6a92f2cb409f6d68
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-11 17:29:26 +0100

    Status code.

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -5,15 +5,15 @@ const request = require('request');
 const url = process.argv[2];
 
 if (!url) {
-    console.error('Usage: node 0-statuscode.js <url>');
+    console.error('Usage: node 0-statuscode.js <https://intranet.alxswe.com/status>');
     process.exit(1);
 }
 
 request(url, (error, response) => {
     if (error) {
-        console.error('Error: ${error.message}');
+        console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 
-    console.log('code: ${response.statusCode}');
+    console.log(`code: ${response.statusCode}`);
 });
