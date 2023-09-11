7fe1b0d662d0931e3e1f7c9d7cf9e998033a73a0
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-11 18:46:01 +0100

    Loripsum.

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+    console.error('Usage: node 3-request_store.js <http://loripsum.net/api loripsum> <loripsum>');
+    process.exit(1);
+}
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+        if (writeError) {
+            console.error(`Error writing to file: ${writeError.message}`);
+            process.exit(1);
+        }
+
+        console.log(`File '${filePath}' has been successfully created and saved.`);       
+    });
+});
