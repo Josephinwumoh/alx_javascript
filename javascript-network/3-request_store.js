467b074e52c2af7991575cb0abb35325d3b2991b
Josephinwumoh <Jinwumoh@gmail.com>
2023-09-11 18:50:36 +0100

    Loripsum.

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -8,7 +8,7 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 if (!url || !filePath) {
-    console.error('Usage: node 3-request_store.js <http://loripsum.net/api loripsum> <file_path>');
+    console.error('Usage: node 3-request_store.js <http://loripsum.net/api loripsum> <3-request_store.js>');
     process.exit(1);
 }
 
